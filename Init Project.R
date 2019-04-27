409437c9b0445217c144940bcb7b1ff635dbaebe
Yoav Reisner <yoavreisner93@gmail.com>
2019-04-27 14:09:01 +0300

    Changed the name from data to dataset for convenience

--- Init Project.R
+++ Init Project.R
@@ -3,7 +3,7 @@ library(data.table)
 library(ggplot2)
 library(tidyverse)
 "Yoav's Computer Directory"
-data <- fread("C:\\Users\\BIGVU\\Documents\\ML Project\\ML-Project\\Data.csv")
+dataset <- fread("C:\\Users\\BIGVU\\Documents\\ML Project\\ML-Project\\Data.csv")
 
 "Yaron's Computer Directory"
 dataset <-  fread("C:\\Users\\Yaron\\Desktop\\working proccess\\Academy\\5\\ML\\projectML\\Data.csv")
