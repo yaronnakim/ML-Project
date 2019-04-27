e3a28a283a341b78f62cd487bfc13126f1af78cc
Yoav Reisner <yoavreisner93@gmail.com>
2019-04-27 11:30:16 +0300

    Library's and data loading

--- 1test.R
+++ 1test.R
@@ -1,2 +1,6 @@
-testaaaa
-"hi"
+library(magrittr)
+library(data.table)
+library(ggplot2)
+library(tidyverse)
+"Yoav's Computer Directory"
+data <- fread("C:\\Users\\BIGVU\\Documents\\ML Project\\ML-Project\\Data.csv")
