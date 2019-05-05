e1b20e9e9b679014e99aa39558e9ec77e85daad6
Yoav Reisner <yoavreisner93@gmail.com>
2019-05-05 11:57:26 +0300

    Changed Directory

--- Init Project.R
+++ Init Project.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(tidyverse)
 library(MASS)
 "Yoav's Computer Directory"
-dataset <- fread("C:\\Users\\BIGVU\\Documents\\ML Project\\ML-Project\\Data.csv")
+dataset <- fread("C:\\Users\\BIGVU\\Documents\\ML Project\\Data.csv")
 
 "Yaron's Computer Directory"
 dataset <-  fread("C:\\Users\\Yaron\\Desktop\\working proccess\\Academy\\5\\ML\\projectML\\Data.csv")
